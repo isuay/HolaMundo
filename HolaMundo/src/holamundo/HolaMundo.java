ab479be09b1742be8ba8bc924be442fe3be996e0
Iris <irisau2@gmail.com>
2021-11-22 19:30:30 +0100

    Creación del archivo Java HolaMundo.

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,24 @@
+/*
+ * Proyecto HolaMundo - Archivo HolaMundo.java - Compañía DAW
+ * Licencia Creative Commons BY-NC-SA 4.0
+ * https://creativecommons.org/licenses/by-nc-sa/4.0/
+ */
+
+package holamundo;
+
+/**
+ *
+ * @author Iris Suay <irisau2@gmail.com>
+ * @version 1.0
+ * @date 22 nov. 2021 19:08:16
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
