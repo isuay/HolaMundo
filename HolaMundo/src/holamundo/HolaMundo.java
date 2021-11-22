dfad1a3fe853aa314329f4a873d9680c74451f08
alumno <alumno@10.3.9.169>
2021-11-22 19:46:36 +0100

    Añadido mensaje de Windows.

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -14,11 +14,8 @@ package holamundo;
  */
 public class HolaMundo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Este mensaje es desde Windows");
     }
-
 }
