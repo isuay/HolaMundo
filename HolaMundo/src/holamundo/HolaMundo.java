1c74e96d8f559fed7ba101565aae114ebda357f1
Iris <irisau2@gmail.com>
2021-11-22 19:32:52 +0100

    Añadimos un mensaje con Hola mundo.

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -18,7 +18,7 @@ public class HolaMundo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hola mundo");
     }
 
 }
